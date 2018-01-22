bc4107718adb35f82b7d0512c8dbc9558a7caf22
Johannes <johannes.ridinger@nordakademie.de>
2018-01-22 20:30:51 +0100

    Updated Models

--- src/main/java/de/nordakademie/iaa/appointment/model/Appointment.java
+++ src/main/java/de/nordakademie/iaa/appointment/model/Appointment.java
@@ -1,4 +1,36 @@
 package de.nordakademie.iaa.appointment.model;
 
+import java.util.Date;
+
 public class Appointment {
+
+   private Date start;
+   private Date end;
+   private Boolean chosen;
+
+   //Constructor
+    public Appointment(Date start, Date end, Boolean chosen) {
+        this.start = start;
+        this.end = end;
+        this.chosen = chosen;
+    }
+
+    //Getter
+
+    public Date getStart() {
+        return start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public Boolean getChosen() {
+        return chosen;
+    }
+
+    //Setter
+    public void setChosen(Boolean chosen) {
+        this.chosen = chosen;
+    }
 }
--- src/main/java/de/nordakademie/iaa/appointmentSurvey/model/AppointmentSurvey.java
+++ src/main/java/de/nordakademie/iaa/appointmentSurvey/model/AppointmentSurvey.java
@@ -1,7 +1,100 @@
 package de.nordakademie.iaa.appointmentSurvey.model;
 
+import de.nordakademie.iaa.appointment.model.Appointment;
+import de.nordakademie.iaa.user.model.User;
+
+import java.util.Date;
+import java.util.List;
+
 public class AppointmentSurvey {
 
-    String title;
-    String description;
+    private String title;
+    private String description;
+    private List<Appointment> appointmentOptions;
+    private Boolean active;
+    private User creator;
+
+    //Constructor
+
+    public AppointmentSurvey(String title, String description, List<Appointment> appointmentOptions, User creator) {
+        this.title = title;
+        this.description = description;
+        this.appointmentOptions = appointmentOptions;
+        this.active = true;
+        this.creator = creator;
+    }
+
+    //Getter
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Appointment> getAppointmentOptions() {
+        return appointmentOptions;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    //setter
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAppointmentOptions(List<Appointment> appointmentOptions) {
+        this.appointmentOptions = appointmentOptions;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    //methods
+
+    //edit Survey
+    public void editSurvey (String title, String description, List<Appointment> options){
+        setTitle(title);
+        setDescription(description);
+        for (int i = 0; i < getAppointmentOptions().size(); i++) {
+            appointmentOptions.remove(i);
+        }
+        setAppointmentOptions(options);
+    }
+
+
+    //end Survey
+    public Appointment getChosenAppointment() {
+        Appointment chosen=null;
+        for (int i = 0; i < getAppointmentOptions().size(); i++) {
+            if (getAppointmentOptions().get(i).getChosen()==true){
+                chosen=getAppointmentOptions().get(i);
+            }
+        }
+        return chosen;
+    }
+
+    public Appointment endSurvey(){
+        setActive(false);
+        return getChosenAppointment();
+    }
+
+
+
 }
+
