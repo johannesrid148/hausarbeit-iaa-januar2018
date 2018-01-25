3d9995e14c552177eeed3bd912a1572c90e4ec65
Max Schumann <maxschumann1@gmail.com>
2018-01-25 14:18:46 +0100

    Klassenerweiterung, Erweiterung DB und web

--- src/main/java/de/nordakademie/iaa/user/model/User.java
+++ src/main/java/de/nordakademie/iaa/user/model/User.java
@@ -2,11 +2,28 @@ package de.nordakademie.iaa.user.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "User")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String userID;
     private String name;
     private String email;
