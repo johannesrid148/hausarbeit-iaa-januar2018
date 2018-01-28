21fcf0f4611ca139eb7c8dbb26e1a899d2b3901b
Max Schumann <maxschumann1@gmail.com>
2018-01-28 15:11:49 +0100

    kleinere Erweiterungen Namespace, etc

--- src/main/java/de/nordakademie/iaa/appointmentSurvey/ui/AppointmentSurveyController.java
+++ src/main/java/de/nordakademie/iaa/appointmentSurvey/ui/AppointmentSurveyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("survey")
+@RequestMapping("/survey")
 public class AppointmentSurveyController {
 
     private final AppointmentSurveyService appointmentSurveyService;
