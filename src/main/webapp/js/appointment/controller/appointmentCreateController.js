9d3c2a6e0f4b0b51dd4801cb17f287d1eb212a87
maxSchumann <maxschumann1@gmail.com>
2018-02-03 23:07:38 +0100

    Detailanzeige Appointments, save appointments, add Date

--- src/main/webapp/js/appointment/controller/appointmentCreateController.js
+++ src/main/webapp/js/appointment/controller/appointmentCreateController.js
@@ -12,10 +12,34 @@ application.controller('appointmentCreateController', [
 
         var surveyID = $routeParams.surveyID;
 
+        $scope.choices = [{id: 'choice1'}, {id: 'choice2'}];
 
+        $scope.addNewChoice = function() {
+            var newItemNo = $scope.choices.length+1;
+            $scope.choices.push({'id':'choice'+newItemNo});
+        };
+
+        $scope.removeChoice = function() {
+            var lastItem = $scope.choices.length-1;
+            $scope.choices.splice(lastItem);
+        };
+
+        $scopte.appoinmentlist = new Array();
+        for (var i = 1; i <= $scope.choices.length; i++){
+            $scope.appoinmentlist.push($scope.appointment)  ;
+        }
 
         $scope.saveAppointment = function () {
-            appointmentService.saveAppointment($scope.appointment, surveyID)
+            console.log("Choices: " + $scope.choices);
+            for (var i in $scope.choices){
+                console.log($scope.appointmentlist[i].start = i.start);
+                ($scope.appointmentlist[i].end = i.end);
+                ($scope.appointmentlist[i].endtime = i.endtime);
+                ($scope.appointmentlist[i].starttime = i.starttime);
+            }
+            console.log("AL: " + $scope.appointmentlist);
+
+            appointmentService.saveAppointment($scope.appointmentlist, surveyID)
                 .then(function (response) {
                     if (response.status === 200) {
                         if (response.data.errorMessage != null) {
